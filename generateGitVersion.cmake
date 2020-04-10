090143940e2e67f280ea6e9021569f6785624af6
aolivier23 <aolivier@ur.rochester.edu>
2020-04-10 13:08:15 -0500

    Encoding git information using a CMake custom target that should always be rebuilt when something changes.

--- generateGitVersion.cmake
+++ generateGitVersion.cmake
@@ -3,6 +3,8 @@
 #       It's a cmake script so that it doesn't depend on platform.
 #Author: Andrew Olivier aolivier@ur.rochester.edu
 
-execute_process(COMMAND git rev-parse --verify HEAD WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_COMMIT_HASH)
-execute_process(COMMAND git status --porcelain WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_UNCOMMITTED_CHANGES)
-configure_file(gitVersion.cpp.in gitVersion.cpp @ONLY)
+message("Hello, world!")
+message("Running in ${REPO_DIR}")
+execute_process(COMMAND git rev-parse --verify HEAD WORKING_DIRECTORY ${REPO_DIR} OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_COMMIT_HASH)
+execute_process(COMMAND git status --porcelain WORKING_DIRECTORY ${REPO_DIR} OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_UNCOMMITTED_CHANGES)
+configure_file(${REPO_DIR}/gitVersion.h.in gitVersion.h @ONLY)
