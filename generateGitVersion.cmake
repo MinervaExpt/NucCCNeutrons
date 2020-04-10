fa40326c619b2ddaabc4bf547a5181796974d444
aolivier23 <aolivier@ur.rochester.edu>
2020-04-10 13:13:11 -0500

    Now, git version embedded in code gets regenerated on every build.

--- generateGitVersion.cmake
+++ generateGitVersion.cmake
@@ -3,8 +3,6 @@
 #       It's a cmake script so that it doesn't depend on platform.
 #Author: Andrew Olivier aolivier@ur.rochester.edu
 
-message("Hello, world!")
-message("Running in ${REPO_DIR}")
-execute_process(COMMAND git rev-parse --verify HEAD WORKING_DIRECTORY ${REPO_DIR} OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_COMMIT_HASH)
-execute_process(COMMAND git status --porcelain WORKING_DIRECTORY ${REPO_DIR} OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_UNCOMMITTED_CHANGES)
-configure_file(${REPO_DIR}/gitVersion.h.in gitVersion.h @ONLY)
+execute_process(COMMAND git rev-parse --verify HEAD WORKING_DIRECTORY OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_COMMIT_HASH)
+execute_process(COMMAND git status --porcelain WORKING_DIRECTORY OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_UNCOMMITTED_CHANGES)
+configure_file(gitVersion.h.in gitVersion.h @ONLY)
