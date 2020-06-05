28c67f565d85c0ba0f55e08fa557d32bff3e04ef
aolivier23 <aolivier@ur.rochester.edu>
2020-06-05 11:14:45 -0500

    First draft of ECAL and HCAL energy cuts.

---
+++ cuts/reco/HCALEnergyMax.cpp
@@ -0,0 +1,18 @@
+//File: ODEnergyMax.cpp
+//Brief: An UpperLimit on energy in MINERvA's Outer Detector.  I'm hoping
+//       to use it to remove events where available energy reconstruction
+//       performs poorly.
+//Author: Andrew Olivier aolivier@ur.rochester.edu
+
+#ifndef RECO_HCALENERGYMAX_CPP
+#define RECO_HCALENERGYMAX_CPP
+
+//cuts includes
+#include "cuts/reco/UpperLimit.h"
+
+namespace
+{
+  static reco::Cut::Registrar<reco::UpperLimit<MeV, &evt::CVUniverse::GetIDHCALEnergy>> reg_ODMax("GetIDHCALEnergy");
+}
+
+#endif //RECO_HCALENERGYMAX_CPP
