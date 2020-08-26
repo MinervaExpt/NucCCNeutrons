167909e0561a2d8a87a85fcd866e7d51bc6b7b9f
aolivier23 <aolivier@ur.rochester.edu>
2020-08-26 12:46:43 -0500

    Added a Study that makes a TTree with per-candidate observables for all candidates with some truth matched information.

---
+++ analyses/studies/PerCandidateTree.cpp
@@ -0,0 +1,115 @@
+//File: PerCandidateTree.cpp
+//Brief: A study on how effectively I detect neutron candidates. Should plot
+//       efficiency to find a FS neutron and a breakdown of fake neutron candidates
+//       in multiple neutron canddiate observables.
+//Author: Andrew Olivier aolivier@ur.rochester.edu
+
+//c++ includes
+#include <cmath>
+
+//signal includes
+#include "analyses/studies/PerCandidateTree.h"
+
+//util includes
+#include "util/Factory.cpp"
+
+using namespace units;
+
+namespace ana
+{
+  PerCandidateTree::PerCandidateTree(const YAML::Node& config, util::Directory& dir, cuts_t&& mustPass, std::vector<background_t>& backgrounds,
+                                     std::map<std::string, std::vector<evt::CVUniverse*>>& univs): Study(config, dir, std::move(mustPass), backgrounds, univs),
+                                                                                                   fCuts(config["variable"])
+  {
+    fMCTree = dir.make<TTree>("MCPerCandidateTree", "One entry for each truth-matched neutron candidate");
+
+    fMCTree->Branch("FSPDG", &fFSPDG);
+    fMCTree->Branch("TruthE", &fTruthE);
+    fMCTree->Branch("TruthAngle", &fTruthAngle);
+
+    fMCTree->Branch("EDep", &fEDep);
+    fMCTree->Branch("DistFromVertex", &fDistFromVertex);
+    fMCTree->Branch("DeltaT", &fDeltaT);
+    fMCTree->Branch("AngleWrtVertex", &fAngleWrtVertex);
+    fMCTree->Branch("NDigits", &fNDigits);
+    fMCTree->Branch("HighestDigitE", &fHighestDigitE);
+    fMCTree->Branch("SmallestAngleDiff", &fSmallestAngleDiff);
+
+    fMCTree->Branch("EventWeight", &fEventWeight);
+  }
+
+  void PerCandidateTree::mcSignal(const evt::CVUniverse& event, const events weight)
+  {
+    fEventWeight = weight.in<events>();
+
+    //Physics objects I'll need
+    const auto cands = event.Get<MCCandidate>(event.Getblob_edep(), event.Getblob_zPos(),
+                                              event.Getblob_transverse_dist_from_vertex(), event.Getblob_earliest_time(),
+                                              event.Getblob_FS_index(), event.Getblob_geant_dist_to_edep_as_neutron(),
+                                              event.Getblob_n_digits(), event.Getblob_highest_digit_E());
+    const auto fs = event.Get<FSPart>(event.GetTruthMatchedPDG_code(), event.GetTruthMatchedenergy(), event.GetTruthMatchedangle_wrt_z());
+    const auto vertex = event.GetVtx();
+
+    for(auto whichCand = cands.begin(); whichCand != cands.end(); ++whichCand)
+    {
+      const auto& cand = *whichCand;
+
+      if(fCuts.countAsReco(cand, vertex))
+      {
+        int pdg = -1; //-1 represents "Other".  Thsi happens when I couldn't find a parent PDG code.
+                      //I think that means either overlay or cross-talk.
+
+        double truthE = -1, truthAngle = -1;
+
+        if(cand.FS_index >= 0)
+        {
+          truthE = fs[cand.FS_index].energy.in<MeV>();
+          truthAngle = fs[cand.FS_index].angle_wrt_z;
+
+          pdg = fs[cand.FS_index].PDGCode;
+          //Check for "GEANT neutron"s: FS particles that weren't neutrons but produced neutrons that I detected.
+          if(pdg != 2112 && cand.dist_to_edep_as_neutron > 0_mm) pdg = std::numeric_limits<int>::max();
+        }
+
+        fFSPDG = pdg;
+        fTruthE = truthE;
+        fTruthAngle = truthAngle;
+
+        const mm deltaZ = cand.z - (vertex.z() - 17_mm); //TODO: 17mm is half a plane width.  Correction for targets?
+        fDistFromVertex = sqrt(pow<2>(cand.transverse) + pow<2>(deltaZ)).in<mm>();
+        fAngleWrtVertex = angle(cand, vertex);
+
+        fEDep = cand.edep.in<MeV>();
+        fDeltaT = cand.time.in<ns>();
+        fNDigits = cand.nDigits;
+        fHighestDigitE = cand.highestDigitE.in<MeV>();
+
+        //Checked that angle() can return a negative number.  Consider deltaZ < 0.
+        auto compareAngle = [this, &vertex](const auto& lhs, const auto& rhs)
+                            {
+                              return fabs(this->angle(lhs, vertex)) < fabs(this->angle(rhs, vertex));
+                            };
+        const auto smallestAnglePrefix = std::min_element(cands.begin(), whichCand, compareAngle);
+        const auto smallestAnglePostfix = std::min_element(whichCand+1, cands.end(), compareAngle);
+        fSmallestAngleDiff = std::numeric_limits<double>::max();
+        if(smallestAnglePrefix != cands.end()) fSmallestAngleDiff = fabs(angle(*smallestAnglePrefix, vertex));
+        if(smallestAnglePostfix != cands.end()) fSmallestAngleDiff = std::min(fSmallestAngleDiff, fabs(angle(*smallestAnglePostfix, vertex)));
+
+        fMCTree->Fill();
+      }
+    }
+  }
+
+
+  double PerCandidateTree::angle(const MCCandidate& cand, const units::LorentzVector<mm>& vertex) const
+  {
+    const mm deltaZ = cand.z - (vertex.z() - 17_mm); //TODO: 17mm is half a plane width.  Correction for targets?
+    return deltaZ.in<mm>() / sqrt(pow<2>(cand.transverse) + pow<2>(deltaZ)).in<mm>();
+  }
+}
+
+//Register with Factory
+namespace
+{
+  static ana::Study::Registrar<ana::PerCandidateTree> PerCandidateTree_reg("PerCandidateTree");
+}
---
+++ analyses/studies/PerCandidateTree.h
@@ -0,0 +1,101 @@
+//File: PerCandidateTree.h
+//Brief: A study on how effectively I detect neutron candidates. Should plot
+//       efficiency to find a FS neutron and a breakdown of fake neutron candidates
+//       in multiple neutron canddiate observables.
+//Author: Andrew Olivier aolivier@ur.rochester.edu
+
+//signal includes
+#include "analyses/base/Study.h"
+
+//variables includes
+#include "analyses/studies/NeutronMultiplicity.cpp"
+
+//util includes
+#include "util/Categorized.h"
+#include "util/units.h"
+#include "util/mathWithUnits.h"
+
+#ifndef SIG_NEUTRONDETECTION_H
+#define SIG_NEUTRONDETECTION_H
+
+namespace ana
+{
+  class PerCandidateTree: public Study
+  {
+    public:
+      PerCandidateTree(const YAML::Node& config, util::Directory& dir, cuts_t&& mustPass,
+                       std::vector<background_t>& backgrounds, std::map<std::string, std::vector<evt::CVUniverse*>>& universes);
+      virtual ~PerCandidateTree() = default;
+
+      //Do this study only for MC signal events.
+      virtual void mcSignal(const evt::CVUniverse& event, const events weight) override;
+
+      //No histograms to normalize
+      virtual void afterAllFiles(const events /*passedSelection*/) override {};
+
+      //Do nothing for backgrounds, the Truth tree, and data
+      virtual void mcBackground(const evt::CVUniverse& /*event*/, const background_t& /*background*/, const events /*weight*/) override {};
+      virtual void truth(const evt::CVUniverse& /*event*/, const events /*weight*/) override {};
+      virtual void data(const evt::CVUniverse& /*event*/) override {};
+
+      //No Truth loop needed
+      virtual bool wantsTruthLoop() const override { return false; }
+
+    private:
+      //Cuts that decide whether a Candidate or FSPart should be counted
+      ana::NeutronMultiplicity fCuts;
+
+      //Format for neutron candidate information.
+      struct NeutronCandidate
+      {
+        MeV edep;
+        mm z;
+        mm transverse;
+        ns time;
+      };
+
+      struct MCCandidate
+      {
+        MeV edep;
+        mm z;
+        mm transverse;
+        ns time;
+        int FS_index; //Mapping from a Candidate to an FSPart by index in the array of FSParts
+        mm dist_to_edep_as_neutron; //Distance parent and ancestors travelled that were neutrons
+        int nDigits;
+        MeV highestDigitE;
+      };
+
+      //Format for FS particle information.
+      struct FSPart
+      {
+        int PDGCode;
+        MeV energy;
+        double angle_wrt_z; //Angle w.r.t. the z axis of the detector in radians
+      };
+
+      //Tree and branches filled for each candidate
+      TTree* fMCTree;
+
+      //Truth-matched branches
+      double fFSPDG;
+      double fTruthE;
+      double fTruthAngle;
+
+      //Reco branches
+      double fEDep;
+      double fDistFromVertex;
+      double fDeltaT;
+      double fAngleWrtVertex;
+      double fNDigits;
+      double fHighestDigitE;
+      double fSmallestAngleDiff;
+
+      //Per-event weight
+      double fEventWeight;
+
+      double angle(const MCCandidate& cand, const units::LorentzVector<mm>& vertex) const;
+  };
+}
+
+#endif //SIG_NEUTRONDETECTION_H
